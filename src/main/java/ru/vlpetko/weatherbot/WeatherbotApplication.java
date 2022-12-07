da22acfbb3a130d57d779dfac6ae0d6133a53883
Владимир Петько <petko.v@yandex.ru>
2022-12-07 19:16:58 +0600

    01_start

---
+++ src/main/java/ru/vlpetko/weatherbot/WeatherbotApplication.java
@@ -0,0 +1,13 @@
+package ru.vlpetko.weatherbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vlpetko/weatherbot/WeatherbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vlpetko.weatherbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
